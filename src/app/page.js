4296437528fd5773536dfd1f20625e81bab49441
taisy03 <107512076+taisy03@users.noreply.github.com>
2025-07-22 12:28:00 +0100

    style changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import Head from 'next/head';
 import Script from 'next/script';
 import '../styles/globals.css';
-import '../styles/fonts.css';
+import '../styles/flowers.css';
 import '../styles/buttons.css';
 import { useState, useEffect } from 'react';
 
@@ -21,20 +21,12 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Taise Sosina</title>
-        <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet" />
-      </Head>
-      
       <main>
         <h1>Taise Sosina</h1>
         <p style={{ textAlign: 'center' }}>Welcome to my portfolio</p>
         <div className="container">
           <button className="start-button" id="startBtn">Start</button>
         </div>
-
-        {/* Pixel Overlay */}
-        <div id="pixelOverlay"></div>
 
         {/* Footer with 6 flower images */}
         <footer className='footer'>
